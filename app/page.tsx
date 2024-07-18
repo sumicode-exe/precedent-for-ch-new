d8f63d6f6ca4661a58df64227731896f4837ecab
Somya Jha <m2somya@gmail.com>
2024-07-18 21:57:25 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <Link href="/api/python">
-            <code className="font-mono font-bold">api/index.py</code>
+            <code className="font-mono font-bold">github.com/thestormbreaks</code>
           </Link>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
@@ -21,7 +21,7 @@ export default function Home() {
             By{" "}
             <Image
               src="/vercel.svg"
-              alt="Vercel Logo"
+              alt=""
               className="dark:invert"
               width={100}
               height={24}
@@ -35,7 +35,7 @@ export default function Home() {
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
-          alt="Next.js Logo"
+          alt=""
           width={180}
           height={37}
           priority
@@ -50,13 +50,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
+            Our Team{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            Find out about out team at cyberherhtz.
           </p>
         </a>
 
@@ -67,13 +67,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
+            Blog{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Look at our previous work 
           </p>
         </a>
 
