4b724349f83be42c1a43b55220f28f28b1989087
Somya Jha <m2somya@gmail.com>
2024-07-19 21:06:29 +0530

    450

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
           //className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/logo.png"
           alt=""
-          width={650}
+          width={450}
           height={100}
           priority
         />
