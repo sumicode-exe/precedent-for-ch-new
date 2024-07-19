2dc84a76ac2332dedcdc72607d45e92d6139fe89
Somya Jha <m2somya@gmail.com>
2024-07-19 21:09:48 +0530

    2

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
           src="/logo.png"
           alt=""
           width={600}
-          height={80}
+          height={200}
           priority
         />
       </div>
