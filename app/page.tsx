83156a29d1895bc422619d622b301887e8b72241
Somya Jha <m2somya@gmail.com>
2024-07-19 18:28:26 +0530

    change?

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Website by&nbsp;
+          Source:&nbsp;
           <Link href="/api/python">
             <code className="font-mono font-bold">github.com/thestormbreaks</code>
           </Link>
@@ -18,6 +18,8 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
+             By Somya Jha{" "}
+            
            
           </a>
         </div>
@@ -28,8 +30,8 @@ export default function Home() {
           //className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/logo.png"
           alt=""
-          width={1200}
-          height={270}
+          width={900}
+          height={170}
           priority
         />
       </div>
