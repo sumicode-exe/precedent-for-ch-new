d5e7728f2098fa93626682b500a88ba7ebd4e28c
Somya Jha <m2somya@gmail.com>
2024-07-19 21:07:32 +0530

    hi

--- app/page.tsx
+++ app/page.tsx
@@ -30,8 +30,8 @@ export default function Home() {
           //className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/logo.png"
           alt=""
-          width={450}
-          height={100}
+          width={200}
+          height={80}
           priority
         />
       </div>
