ecd086b34aecff815ebb297c8788726c68aa7324
Somya Jha <m2somya@gmail.com>
2024-07-20 10:05:10 +0530

    team page added

--- app/teampage/teampage.tsx
+++ app/teampage/teampage.tsx
@@ -18,7 +18,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           ><a href = "http://www.somya.cyberhertz.in">
-             teAM PAGE{" "}
+             By Somya Jha{" "}
             </a>
            
           </a>
@@ -38,7 +38,7 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="app\teampage\teampage.tsx"
+          href="http://www.team.cyberhertz.in"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
