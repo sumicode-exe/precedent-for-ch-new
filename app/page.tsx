9964e244e2a4cd754c067b99e439165add082b21
Somya Jha <m2somya@gmail.com>
2024-07-20 10:20:10 +0530

    up

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -37,22 +40,24 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
+      <Link to="/teampage"><a
           href="app\teampage\teampage.tsx"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Our Team{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find out more about our leaders and their journey.
-          </p>
-        </a>
+        >Go to Team Page
+        <h2 className={`mb-3 text-2xl font-semibold`}>
+        Our Team{" "}
+        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+          -&gt;
+        </span>
+      </h2>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+        Find out more about our leaders and their journey.
+      </p>
+    </a></Link>
+        
+          
 
         <a
           href="http://www.projects.cyberhertz.in"
