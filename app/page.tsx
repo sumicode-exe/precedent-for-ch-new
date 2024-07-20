8c215db9f088d798d5842d3dc33ba42debae5b9d
Somya Jha <m2somya@gmail.com>
2024-07-20 10:21:32 +0530

    link try

--- app/page.tsx
+++ app/page.tsx
@@ -40,24 +40,18 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-      <Link to="/teampage"><a
-          href="app\teampage\teampage.tsx"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >Go to Team Page
-        <h2 className={`mb-3 text-2xl font-semibold`}>
-        Our Team{" "}
-        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-          -&gt;
-        </span>
-      </h2>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-        Find out more about our leaders and their journey.
-      </p>
-    </a></Link>
+      <Link to="/teampage">Go to Team Page</Link>
         
-          
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Our Team{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find out more about our leaders and their journey.
+          </p>
+        </a>
 
         <a
           href="http://www.projects.cyberhertz.in"
